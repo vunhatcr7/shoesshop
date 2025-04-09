2cf4d97218ef4616eace8d63fdaa65cc1e9d1670
vunhatcr7 <134484885+vunhatcr7@users.noreply.github.com>
2025-04-09 11:03:37 +0700

    Update LoginAdmin.js

--- views/LoginAdmin.js
+++ views/LoginAdmin.js
@@ -51,34 +51,7 @@ class Login extends React.Component {
 
     render(){
     return(
-        // <>  
-        // <h1 className="flex item-center justify-center bg-blue-500 text-5xl font-bold  "> Welcome to my shop</h1>
-        //    <div className="flex item-center justify-center  bg-blue-500">         
-        //     <form className=" mt-20 bg-white p-6 shadow-lg rounded-lg space-y-4 w-96">
-        //         <div>
-        //             <label className ="block text-grey-700">Username</label>
-        //             <input type="username" value={this.state.username} className="w-full border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500"
-        //                 onChange={(e) => this.handleChangeUser(e)}
-        //             />
-        //         </div>
-        //         <div>
-        //             <label className="block text-grey-700">Password</label>
-        //             <input type="password" value={this.state.password} className="w-full border-gray-300 p-2 rounded-lg focus:ring-2 focus:ring-blue-500"
-        //                 onChange={(e) => this.handleChangePassword(e)}
-        //             />
-        //         </div>
-        //         <Button className="w-full bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-700"
-        //             onClick={(e) => this.handleLogin(e)}>
-        //             Submit
-        //         </Button>
-        //         <Button  className="w-full bg-blue-500 text-white p-2 rounded-lg hover:bg-blue-700"
-        //             onClick={(e) => this.handleRegister(e)}>
-        //             Register Now
-        //         </Button>
-        //     </form>
-        //    </div>
-        // </>
-
+     
         <div className="relative h-screen flex items-center justify-center bg-cover bg-center" style={{backgroundImage: `url(${BG})`}}>
             <div className="bg-gray-900 bg-opacity-75 p-8 rounded-xl shadow-lg max-w-md w-full">
                 <h2 className="text-white text-2xl font-semibold text-center mb-4">Sign in your account</h2>
